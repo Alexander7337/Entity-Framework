dcf99ce337b209617a76b91d27afd201bcccb388
Alexander7337 <yanev.alexander@gmail.com>
2016-11-08 08:46:38 +0200

    Clean code

--- EFCodeFirstPractice/EFCodeFirstPractice.Data/CodeFirstPracticeContext.cs
+++ EFCodeFirstPractice/EFCodeFirstPractice.Data/CodeFirstPracticeContext.cs
@@ -8,23 +8,12 @@ namespace EFCodeFirstPractice.Data
 
     public class CodeFirstPracticeContext : DbContext
     {
-        // Your context has been configured to use a 'CodeFirstPracticeEntities' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'EFCodeFirstPractice.Data.CodeFirstPracticeEntities' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'CodeFirstPracticeEntities' 
-        // connection string in the application configuration file.
         public CodeFirstPracticeContext()
             : base("CodeFirstPracticeContext")
         {
             var migrationStrategy = new MigrateDatabaseToLatestVersion<CodeFirstPracticeContext, MyConfiguration>();
             Database.SetInitializer(migrationStrategy);
         }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
 
         public IDbSet<WizardDeposit> WizardDeposits { get; set; }
 
@@ -34,10 +23,4 @@ namespace EFCodeFirstPractice.Data
 
         public IDbSet<Country> Countries { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
--- EFCodeFirstPractice/EFCodeFirstPractice.Models/User.cs
+++ EFCodeFirstPractice/EFCodeFirstPractice.Models/User.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Policy;
-using System.Web.Security;
-using EFCodeFirstPractice.Models.Attributes;
-
-
-namespace EFCodeFirstPractice.Models
+﻿namespace EFCodeFirstPractice.Models
 {
     using System;
     using System.ComponentModel.DataAnnotations;
+    using EFCodeFirstPractice.Models.Attributes;
+    using System.ComponentModel.DataAnnotations.Schema;
     public class User
     {
         [Key]
@@ -21,16 +17,21 @@ namespace EFCodeFirstPractice.Models
         public string Username { get; set; }
 
         [Required]
+
         //[Password(4, 20, true, true, true, true)]
+
         [Pass(4, 20, ContainsLowercase = true, ContainsUppercase = true, ContainsDigit = true, ContainsSpecialSymbol = true)]
+        
         //[RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{4,}$", 
         //    ErrorMessage="Password should have at least 1 small letter, 1 capital letter, 1 digit and 1 special character")]
         public string Password { get; set; }
 
         [Required]
+
         //[RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", 
         //    ErrorMessage = "Invalid email address")]
         //[CustomValidation(typeof(Email), "ValidateEmail")]
+
         [Email]
         public string Email { get; set; }
 
