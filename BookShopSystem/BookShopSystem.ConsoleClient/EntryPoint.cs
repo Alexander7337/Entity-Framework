3ad50364442cf20f5ac5ac567e7f26b4bf7e5bfe
Alexander7337 <yanev.alexander@gmail.com>
2016-11-18 14:27:27 +0200

    Additional tasks

--- BookShopSystem/BookShopSystem.ConsoleClient/EntryPoint.cs
+++ BookShopSystem/BookShopSystem.ConsoleClient/EntryPoint.cs
@@ -5,6 +5,7 @@
     using BookShopSystem.Data;
     using System.Collections.ObjectModel;
     using System.Data.Entity;
+    using System.Collections.Generic;
     using BookShopSystem.Data.Migrations;
     using EntityFramework.Extensions;
     using Models;
@@ -111,6 +112,159 @@
             //    .ToArray();
 
             //Array.ForEach(titles, t => Console.WriteLine(t));
+            #endregion
+
+            #region 2.Golden Books
+            //string[] titles = context.Books
+            //    .Where(e => e.EditionType == EditionType.Gold
+            //                && e.Copies < 5000)
+            //    .Select(e => e.Title)
+            //    .ToArray();
+
+            //Array.ForEach(titles, t => Console.WriteLine(t));
+            #endregion
+
+            #region 3.Books by Price
+            //var books = context.Books
+            //    .Where(e => e.Price < 5 || e.Price > 40)
+            //    .Select(e => new
+            //    {
+            //        e.Title,
+            //        e.Price
+            //    })
+            //    .ToArray();
+
+            //Array.ForEach(books, book => Console.WriteLine(book.Title + " - " + book.Price));
+            #endregion
+
+            #region 4.Not Released Books
+            //int year = int.Parse(Console.ReadLine());
+
+            //string[] titles = context.Books
+            //    .Where(e => e.ReleaseDate.Value.Year != year)
+            //    .Select(e => e.Title)
+            //    .ToArray();
+
+            //Array.ForEach(titles, t => Console.WriteLine(t));
+            #endregion
+
+            #region 5.Book Titles by Category
+            //string[] categories = Console.ReadLine()
+            //    .ToLower()
+            //    .Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+
+            //var booksCategorized = context.Categories
+            //    .Where(e => categories.Contains(e.Name.ToLower()))
+            //    .Select(e => e.Books)
+            //    .ToArray();
+
+            //foreach (var element in booksCategorized)
+            //{
+            //    foreach (var book in element)
+            //    {
+            //        Console.WriteLine(book.Title);
+            //        foreach (var category in book.Categories)
+            //        {
+            //            Console.WriteLine($"--{category.Name}");
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region 6.Books Released Before Date
+            //DateTime date = DateTime.Parse(Console.ReadLine());
+
+            //var books = context.Books
+            //    .Where(e => e.ReleaseDate.Value.CompareTo(date) == -1)
+            //    .Select(e => new
+            //    {
+            //        e.Title,
+            //        e.EditionType,
+            //        e.Price
+            //    })
+            //    .ToArray();
+
+            //Array.ForEach(books, book => Console.WriteLine(book.Title));
+            #endregion
+
+            #region 7.Book Titles Search
+            //string input = Console.ReadLine();
+
+            //var books = context.Books
+            //    .Where(e => e.Author.LastName.StartsWith(input))
+            //    .Select(e => new
+            //    {
+            //        e.Title,
+            //        e.Author.FirstName,
+            //        e.Author.LastName
+            //    })
+            //    .ToArray();
+
+            //Array.ForEach(books, book => Console.WriteLine($"{book.Title} ({book.FirstName} {book.LastName})"));
+            #endregion
+
+            #region 8.Total Book Copies
+            //var stats = context.Books
+            //    .GroupBy(e => e.Author)
+            //    .OrderByDescending(e => e.Select(n => n.Copies).Sum())
+            //    .Select(e => new
+            //    {
+            //        Author = e.Key,
+            //        Copies = e.Select(n => n.Copies).Sum()
+            //    });
+
+            //foreach (var author in stats)
+            //{
+            //    Console.WriteLine($"{author.Author.FirstName} {author.Author.LastName} - {author.Copies}");
+            //}
+            #endregion
+
+            #region 9.Find Profit
+            //var booksByCategories = context.Categories
+            //    .Select(e => new
+            //    {
+            //        e.Name,
+            //        e.Books
+            //    });
+
+            //foreach (var booksByCategory in booksByCategories)
+            //{
+            //    decimal moneyPerCategory = 0m;
+            //    foreach (var book in booksByCategory.Books)
+            //    {
+            //        moneyPerCategory += (book.Price*book.Copies);
+            //    }
+
+            //    Console.WriteLine($"{booksByCategory.Name} - ${moneyPerCategory}");
+            //}
+            #endregion
+
+            #region 10.Most Recent Books
+            //var top3books = context.Categories
+            //    .OrderByDescending(e => e.Books.Count)
+            //    .Select(e => new
+            //    {
+            //        e.Name,
+            //        Number = e.Books.Count,
+            //        Books = e.Books
+            //        .OrderByDescending(b => b.ReleaseDate.Value.Year)
+            //        .ThenBy(t => t.Title)
+            //        .Take(3)
+            //        .Select(b => new
+            //        {
+            //            b.Title,
+            //            b.ReleaseDate
+            //        })
+            //    });
+
+            //foreach (var category in top3books)
+            //{
+            //    Console.WriteLine($"--{category.Name}: {category.Number} books");
+            //    foreach (var book in category.Books)
+            //    {
+            //        Console.WriteLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
+            //    }
+            //}
             #endregion
         }
     }
